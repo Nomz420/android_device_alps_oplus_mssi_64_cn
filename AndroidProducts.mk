ebb35352fbbb6192b404fd5ff5c64175649c6b98
Nomz <42914692+Nomz420@users.noreply.github.com>
2022-02-17 12:43:28 +0100

    added the device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_oplus_mssi_64_cn.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_oplus_mssi_64_cn-user \
+    omni_oplus_mssi_64_cn-userdebug \
+    omni_oplus_mssi_64_cn-eng
---
+++ omni_oplus_mssi_64_cn.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from oplus_mssi_64_cn device
+$(call inherit-product, device/alps/oplus_mssi_64_cn/device.mk)
+
+# Inherit some common omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := oplus_mssi_64_cn
+PRODUCT_NAME := omni_oplus_mssi_64_cn
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := oplus_mssi_64_cn
+PRODUCT_MANUFACTURER := alps
